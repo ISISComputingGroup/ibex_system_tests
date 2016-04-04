dc62baa9f70547e7405853cc66e1651b8b5ade52
Ian Bush <ian.bush@stfc.ac.uk>
2016-04-04 17:35:27 +0100

    Try setting password before copy

--- jenkins.bat
+++ jenkins.bat
@@ -1,4 +1,10 @@
 md EPICS
+
+REM the password for isis\builder is contained in the BUILDERPW system environment variable on the build server
+REM we map this early as some other stuff (e.g. CSS, DAE DLLs) is copied from \\isis\inst$ too during build 
+net use p: /d
+net use p: \\isis\inst$ /user:isis\builder %BUILDERPW%
+
 XCOPY "\\isis\inst$\Kits$\CompGroup\ICP\EPICS\EPICS_CLEAN_win7_x64\BUILD-1101" EPICS /C /S /D /Y /I
 
 cd EPICS
