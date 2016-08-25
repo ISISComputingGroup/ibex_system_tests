dafc2f16ae0050f4ca8dc412ac747b7660e839fb
IsabellaRey <isabella.rey@stfc.ac.uk>
2016-08-25 11:55:07 +0100

    Ticket 1436: added commented code for importing version control library, but this is just temporary

--- RCPTT_Tests/cleanIBEXServer/cleanIBEXServer.py
+++ RCPTT_Tests/cleanIBEXServer/cleanIBEXServer.py
@@ -1,16 +1,27 @@
 """
 Script to clean up files in the IBEX server between tests
 """
-
 import os
+import sys
+#
+# # Path to EPICS
+# default_epics_path = os.path.join("C:\\", "Instrument", "Apps", "EPICS")
+# EPICS = os.environ("MYDIR", default_epics_path)
+#
+# # Path to version control
+# VC_PATH = os.path.join(EPICS, "ConfigVersionControl", "master")
+# sys.path.insert(0, os.path.abspath(VC_PATH))
+
 import stat
 import subprocess
 import shutil
 from subprocess import PIPE
 from time import sleep
+# from config_version_control import ConfigVersionControl
 
 import errno
 import psutil
+
 
 # How often should be clean the directories
 RUN_CLEAN_PER = 0
