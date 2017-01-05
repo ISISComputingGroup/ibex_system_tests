50f1325d47ab5d0b053475e82cc3ad9cad229be0
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-01-05 18:34:15 +0000

    Improve cleanup retry

--- jenkins.bat
+++ jenkins.bat
@@ -23,10 +23,18 @@ if exist "C:\Instrument\Apps\EPICS\stop_ibex_server.bat" (
 
 REM Delete simulated instrument
 rd /S /Q "C:\data"
+if exist "C:\data" (
+    timeout /t 60 /nobreak >NUL
+    rd /S /Q "C:\data"
+)
 mkdir "C:\data"
 
 REM Install genie_python, deleting the old one first, and going back to the workspace that the installer moves from
 rd /S /Q "C:\Instrument\Apps\Python\"
+if exist "C:\Instrument\Apps\Python\" (
+    timeout /t 10 /nobreak >NUL
+    rd /S /Q "C:\Instrument\Apps\Python\"
+)
 
 call "\\isis\inst$\Kits$\CompGroup\ICP\Client\genie_python\genie_python_install.bat"
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -35,10 +43,21 @@ cd %WORKSPACE%
 
 REM Clean up the previous versions
 rd /S /Q "C:\Instrument\Apps\EPICS\"
-if exist "C:\Instrument\Apps\EPICS\" rd /S /Q "C:\Instrument\Apps\EPICS\"
+if exist "C:\Instrument\Apps\EPICS\" (
+    timeout /t 10 /nobreak >NUL
+    rd /S /Q "C:\Instrument\Apps\EPICS\"
+)
+if exist "C:\Instrument\Apps\EPICS\" (
+    timeout /t 60 /nobreak >NUL
+    rd /S /Q "C:\Instrument\Apps\EPICS\"
+)
 
 REM Delete old configuration directory
 rd /S /Q "C:\Instrument\Settings\"
+if exist "C:\Instrument\Settings\" (
+    timeout /t 10 /nobreak >NUL
+    rd /S /Q "C:\Instrument\Settings\"
+)
 
 REM Delete all but old logs as these are useful for debugging
 for /D %%d in ( C:\Instrument\Var\* ) do (
@@ -49,8 +68,10 @@ for /D %%d in ( C:\Instrument\Var\* ) do (
 
 REM Clean up the previous version of the GUI
 rd /S /Q "ibex_gui"
-if exist "ibex_gui" rd /S /Q "ibex_gui"
-
+if exist "ibex_gui" (
+    timeout /t 10 /nobreak >NUL
+    rd /S /Q "ibex_gui"
+)
 REM Get the latest versions via a Python script
 c:\Python27\python.exe get_latest_builds.py
 if %errorlevel% neq 0 exit /b %errorlevel%
