bbce29ad36ddd9432ddc14698be5e4f4b314dc10
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-04-06 09:36:57 +0100

    Make neater

--- get_latest_builds.py
+++ get_latest_builds.py
@@ -12,20 +12,18 @@ EPICS_BUILD_FOLDER_PATTERN = "BUILD-(\d+)"
 version = 0
 folder = None
 for x in os.listdir(EPICS_KITS_DIR):
-    if os.path.isfile(os.path.join(EPICS_KITS_DIR, x, 'EPICS', 'COPY_COMPLETE.txt')):
-        m = re.match(EPICS_BUILD_FOLDER_PATTERN, x)
-        if m is not None:
+    m = re.match(EPICS_BUILD_FOLDER_PATTERN, x)
+    if m is not None:
+        if os.path.isfile(os.path.join(EPICS_KITS_DIR, x, 'EPICS', 'COPY_COMPLETE.txt')):
             d = int(m.groups()[0])
             if d > version:
                 version = d
                 folder = x
-    else:
-        m = re.match(EPICS_BUILD_FOLDER_PATTERN, x)
-        if m is not None:
+        else:
             print "Warning: COPY_COMPLETE not found for build " + m.groups()[0]
             print "This might be because the build is incomplete, or because the folder structure has changed."
 			
-
+			
 if folder is not None: 
     print "Copying EPICS from %s, please wait..." % folder
     shutil.copytree("%s\\%s" % (EPICS_KITS_DIR, folder), "C:\\Instrument\\Apps\\EPICS")
