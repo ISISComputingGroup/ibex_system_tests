fd49ebe02aa69c45b0be216638297b12b6ef3fe9
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2017-05-20 23:27:31 +0100

    Run from workspace directory

--- jenkins_ioctests.bat
+++ jenkins_ioctests.bat
@@ -11,9 +11,9 @@ REM call "C:\Instrument\Apps\EPICS\config_env"
 REM Get the icp binaries so that the DAE can run
 REM call "C:\Instrument\Apps\EPICS\create_icp_binaries"
 
-cd C:\Instrument\Apps\EPICS\support\IocTestFramework\master
+rmdir /q /s test-reports
 
-call run_all_tests.bat
+call "C:\Instrument\Apps\EPICS\support\IocTestFramework\master\run_all_tests.bat"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM cleanup
